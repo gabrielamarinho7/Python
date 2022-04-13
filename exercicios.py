cfd4513bbdedcba6da5f895ba1f78ee9b5c2818f
Gabriela Marinho <marinhogabriela@outlook.com.br>
2022-04-12 21:51:13 -0300

    Create exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,49 @@
+# Crie três tipos de variáveis diferentes
+
+nome = 'Gabriela'
+idade = 27
+valor = 19.99
+
+# Escreva um programa que mostra a mensagem: Olá Mundo!
+
+print("Olá Mundo!")
+
+# Crie uma variável Nome e atribua para a mesma um nome digitado pelo usuário
+
+Nome = input('Por favor, digite seu nome: ')
+
+# Exiba em tela o valo e o tipo de dado da variável num1, sendo num1 = 1987
+
+num1 = 1987
+
+print(num1)
+print(type(num1))
+
+# Peça ao usuário para digitar um número e depois exiba em tela o número digitado
+
+num = input('Digite um número: ')
+
+print(f'O número digitado é:  {num}')
+
+# Peça que o usuário digite um número, em seguida o converta para float, exibindo em tela o número e o tipo de dado
+
+num = input('Digite um número: ')
+
+num = float(num)
+
+print(num)
+print(type(num))
+
+# Crie uma lista com 5 nomes de pessoas // Lembrando que listas tem seus dados dentro de colchetes []
+
+nomes = ['Gabriela', 'Isabhela', 'Luiz', 'Maria', 'Clara']
+
+print(nomes)
+
+# Crie uma tupla com 3 nomes de pessoas
+
+tupla_nomes = ('Patricia', 'Izaque', 'Isabhela')
+
+print(type(tupla_nomes))
+
+#Parei na página 758 do livro Python Na Prática
